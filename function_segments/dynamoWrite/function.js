075e859c659b716a49066da6c83a0be5d45bafc2
AZ <m.ahsanziak@gmail.com>
2026-03-19 18:42:28 -0600

    Migrate all AWS references to us-west-2 and update runtime to nodejs16

--- function_segments/dynamoWrite/function.js
+++ function_segments/dynamoWrite/function.js
@@ -2,7 +2,7 @@ async function func() {
   // START FUNCTION
   const { v4: uuidv4 } = require('uuid');
   const AWS = require("aws-sdk");
-  const dynamodb = new AWS.DynamoDB({ region: "eu-west-1" });
+  const dynamodb = new AWS.DynamoDB({ region: "us-west-2" });
 
   const numOfRequests = ~~(Math.random() * 3) + 1;
   for (let j = 0; j < numOfRequests; j++) {
